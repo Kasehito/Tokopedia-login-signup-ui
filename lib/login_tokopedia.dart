0b9b27defc4dfa731f9ebd007059f6287cc76534
Royyan Zaydan <77264514+Kasehito@users.noreply.github.com>
2024-08-08 21:51:39 +0700

    initial commit

---
+++ lib/login_tokopedia.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginTokopedia extends StatefulWidget {
+  const LoginTokopedia({super.key});
+
+  @override
+  State<LoginTokopedia> createState() => _LoginTokopediaState();
+}
+
+class _LoginTokopediaState extends State<LoginTokopedia> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
